2b9766f360df3c565f091f86f4b0bfdaf6960ae2
jq1004 <junqingl@usc.edu>
2019-05-29 13:22:46 -0400

    getPostsByUser

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts,createPost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser} = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const {createPostValidator} = require("../validator");
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+
+router.get("/posts/by/:userId",requireSignin, postsByUser);
 
 // any route containing: userId, out app will first execute userByID()
 router.param("userId",userById);
