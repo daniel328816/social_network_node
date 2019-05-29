96fc23e4d3a09d1fbd4d79fcabd0ba927fbaf175
jq1004 <junqingl@usc.edu>
2019-05-29 12:30:23 -0400

    User&Post relationship

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,4 +1,6 @@
 const Post = require("../models/post");
+const formidable = require('formidable');
+const fs = require('fs');
 
 exports.getPosts = (req, res) => {
 	const posts = Post.find()
@@ -10,21 +12,32 @@ exports.getPosts = (req, res) => {
 };
 
 exports.createPost = (req, res) => {
-	const post = new Post(req.body);
-	// console.log("CREATING POST: ", req.body);
-	// post.save((err, result) => {
-	// 	if (err){
-	// 		return res.status(400).json({
-	// 			error: err
-	// 		});
-	// 	}
-	// 	res.status(200).json({
-	// 		post: result
-	// 	});
-	// });
-	post.save().then(result => {
-		res.json({
-			post:result
+	let form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+	form.parse(req, (err, fields, files) => {
+		if(err){
+			return res.status(400).json({
+				error: "Image could not be uploaded"
+			});
+		}
+
+		let post = new Post(fields);
+		req.profile.hashed_password = undefined;
+		req.profile.salt = undefined;
+		post.postedBy = req.profile;
+		if(files.photo){
+			post.photo.data = fs.readFileSync(files.photo.path);
+			post.photo.contentType = files.photo.type;
+		}
+		post.save((err,result)=>{
+			if(err){
+				return res.status(400).json({
+					error: err
+				});
+			}
+			res.json(result);
 		});
+
 	});
+
 };
