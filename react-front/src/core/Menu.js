fa2b2678343fcf54e8642198d8d9cdc49c42a568
jq1004 <junqingl@usc.edu>
2019-05-30 16:19:27 -0400

    show user info from local storage

--- react-front/src/core/Menu.js
+++ react-front/src/core/Menu.js
@@ -33,11 +33,9 @@ const Menu = ({history}) => (
 					</li>
 
 					<li className="nav-item">	
-				   		<a className="nav-link">
-				   			<Link to={`/user/${isAuthenticated().user._id}`} style={{color:"#fff"}}>
-				   				{`${isAuthenticated().user.name}'s profile`}
-				   			</Link>
-				   		</a>
+				   		<Link to={`/user/${isAuthenticated().user._id}`} style={{color:"#fff"}} className="nav-link">
+				   			{`${isAuthenticated().user.name}'s profile`}
+				   		</Link>
 				   		
 					</li>
 				</>
