8351a7c275029017293c2973995a5e1f7d233717
jq1004 <junqingl@usc.edu>
2019-05-30 10:10:43 -0400

    code refactoring signup

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -54,22 +54,10 @@ class Signup extends Component {
 				return response.json();
 			})
 			.catch(err => console.log(err));
-	}
+	};
 
-
-	render(){
-		const {name, email, password, error, open} = this.state;
-		return (
-			 <div className="container">
-			 	<h2 className="mt-5 mb-5">Signup</h2>
-
-			 	<div className="alert alert-primary" style={{display: error ? "": "none"}}>
-			 		{error}
-			 	</div>
-			 	<div className="alert alert-info" style={{display: open ? "": "none"}}>
-			 		New account is succesfully created. Please Sign In.
-			 	</div>
-			 	<form>
+	signupForm = (name, email, password) => (
+			<form>
 			 		<div className="form-group">
 			 			<label className="text-muted">Name</label>
 			 			<input onChange={this.handleChange("name")} type="text" className="form-control" value={name} />
@@ -84,6 +72,20 @@ class Signup extends Component {
 			 		</div>
 			 		<button onClick={this.clickSubmit} className="btn btn-raised btn-primary">Submit</button>
 			 	</form>
+		)
+	render(){
+		const {name, email, password, error, open} = this.state;
+		return (
+			 <div className="container">
+			 	<h2 className="mt-5 mb-5">Signup</h2>
+
+			 	<div className="alert alert-danger" style={{display: error ? "": "none"}}>
+			 		{error}
+			 	</div>
+			 	<div className="alert alert-info" style={{display: open ? "": "none"}}>
+			 		New account is succesfully created. Please Sign In.
+			 	</div>
+			 	{this.signupForm(name, email, password)}
 			 </div>
 		);
 	}
