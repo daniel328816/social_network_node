dee79a6856f42c826580872ce4d144b83ed630d7
jq1004 <junqingl@usc.edu>
2019-05-30 21:18:09 -0400

    loop through users

--- react-front/src/user/Users.js
+++ react-front/src/user/Users.js
@@ -20,9 +20,18 @@ class Users extends Component {
 	}
 
 	render(){
+		const {users} = this.state;
 		return (
 			<div className="container">
 				<h2 className="mt-5 mb-5">Users</h2>
+				<div className="card">
+					{
+						users.map((user,i) => (
+						  <div key={i}>
+							<p>{user.name}</p>
+						  </div>						
+					))}
+				</div>
 			</div>
 		);
 	}
