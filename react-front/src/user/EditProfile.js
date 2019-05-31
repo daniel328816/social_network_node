1a1623bb9204f847e7bfeb0835a0036954df515f
jq1004 <junqingl@usc.edu>
2019-05-31 10:14:35 -0400

    profile photo upload

--- react-front/src/user/EditProfile.js
+++ react-front/src/user/EditProfile.js
@@ -36,6 +36,7 @@ class EditProfile extends Component {
 	};
 
 	componentDidMount(){
+		this.userData = new FormData();
 		const userId = this.props.match.params.userId;
 		this.init(userId);
 			
@@ -60,7 +61,9 @@ class EditProfile extends Component {
 	};
 
 	handleChange = name => event => {
-		this.setState({[name]: event.target.value});
+		const value = name === 'photo' ? event.target.files[0] : event.target.value;
+		this.userData.set(name, value);
+		this.setState({[name]: value});
 	};
 
 	clickSubmit = event => {
@@ -77,7 +80,7 @@ class EditProfile extends Component {
 			const token = isAuthenticated().token;
 			const userId = this.props.match.params.userId;
 
-			update(userId, token, user).then(data => {
+			update(userId, token, this.userData).then(data => {
 				if (data.error) this.setState({error: data.error});
 				else 
 					this.setState({
@@ -90,6 +93,16 @@ class EditProfile extends Component {
 
 	signupForm = (name, email, password) => (
 			<form>
+					<div className="form-group">
+			 			<label className="text-muted">Profile Photo</label>
+			 			<input 
+			 			onChange={this.handleChange("photo")} 
+			 			type="file" 
+			 			accept="image/*"
+			 			className="form-control" 
+			 			 />
+			 		</div>
+
 			 		<div className="form-group">
 			 			<label className="text-muted">Name</label>
 			 			<input 
--- react-front/src/user/apiUser.js
+++ react-front/src/user/apiUser.js
@@ -14,14 +14,14 @@ export const read = (userId, token) => {
 	};
 
 export const update = (userId, token, user) => {
+		console.log("USER DATA UPDATE: ",user);
 		return fetch(`http://localhost:8080/user/${userId}`,{
 			method:'PUT',
 			headers: {
 				Accept:"application/json",
-				"Content-Type": "application/json",
 				Authorization: `Bearer ${token}`
 			},
-			body: JSON.stringify(user)
+			body: user
 		})
 		.then(response => {
 			return response.json();
