00f42da3bcd9b890dd64c9847e5dde179ace5d98
jq1004 <junqingl@usc.edu>
2019-05-30 22:29:15 -0400

    Delete profile component

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -3,6 +3,7 @@ import {isAuthenticated} from '../auth';
 import { Redirect, Link } from "react-router-dom";
 import {read} from "./apiUser";
 import DefaultProfile from '../images/avatar.jpg';
+import DeleteUser from './DeleteUser';
 
 class Profile extends Component{
 
@@ -65,9 +66,7 @@ class Profile extends Component{
 									<Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
 									Edit Profile
 									</Link>
-									<button className="btn btn-raised btn-danger">
-										Delete Profile
-									</button>
+									<DeleteUser />
 								</div>
 
 								)}
