db9c28cb514caf96dd67ef5530a1ea635f88e510
jq1004 <junqingl@usc.edu>
2019-06-02 13:09:44 -0400

    follow profile buttons component

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -4,6 +4,7 @@ import { Redirect, Link } from "react-router-dom";
 import {read} from "./apiUser";
 import DefaultProfile from '../images/avatar.jpg';
 import DeleteUser from './DeleteUser';
+import FollowProfileButton from './FollowProfileButton';
 
 class Profile extends Component{
 
@@ -65,7 +66,7 @@ class Profile extends Component{
 								<p>Email: {user.email}</p>
 								<p>{`Joined ${new Date(user.created).toDateString()}`}</p>
 							</div>
-							{isAuthenticated().user && isAuthenticated().user._id === user._id && (
+							{isAuthenticated().user && isAuthenticated().user._id === user._id ? (
 								<div className="d-inline-block">
 									<Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
 									Edit Profile
@@ -73,6 +74,8 @@ class Profile extends Component{
 									<DeleteUser userId={user._id}/>
 								</div>
 
+								) : (
+									<FollowProfileButton />
 								)}
 						</div>
 					</div>	
