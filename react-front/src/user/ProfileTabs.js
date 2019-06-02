8eb7d86193f27ba4f257f48e1c874073530481b7
jq1004 <junqingl@usc.edu>
2019-06-02 16:16:53 -0400

    display following list

--- react-front/src/user/ProfileTabs.js
+++ react-front/src/user/ProfileTabs.js
@@ -14,28 +14,60 @@ class ProfileTabs extends Component {
 
 						{followers.map((person,i) => 
 							 (<div key={i}>
-								<div className="row">
+								
 									<div>
 										<Link to={`/user/${person._id}`}>
 											<img 
+												style={{borderRadius: "50%", border: "1px solid black"}}
 												className="float-left mr-2"
 												height="30px"
+												width="30px"
 												onError={ i => (i.target.src = `${DefaultProfile}`)}
 												src={`http://localhost:8080/user/photo/${person._id}`}
 												alt={person.name}
 											/>
 											<div>
-												<h3>{person.name}</h3>
+												<p className="lead">{person.name}</p>
 											</div>
 										</Link>
-										<p style={{clear:'both'}}>
-											{person.about}
-										</p>
 									</div>
-								</div>
+								
 							</div>
 							)
 						)}
+					</div>
+					<div className="col-md-4"> 
+						<h3 className="text-primary">Following</h3>
+						<hr/>
+
+						{following.map((person,i) => 
+							 (<div key={i}>
+								
+									<div>
+										<Link to={`/user/${person._id}`}>
+											<img 
+												style={{borderRadius: "50%", border: "1px solid black"}}
+												className="float-left mr-2"
+												height="30px"
+												width="30px"
+												onError={ i => (i.target.src = `${DefaultProfile}`)}
+												src={`http://localhost:8080/user/photo/${person._id}`}
+												alt={person.name}
+											/>
+											<div>
+												<p className="lead">{person.name}</p>
+											</div>
+										</Link>
+			
+									</div>
+								
+							</div>
+							)
+						)}
+					</div>
+					<div className="col-md-4">
+						<h3 className="text-primary">Posts</h3>
+						<hr/>
 					</div>
 				</div>
 			</div>
