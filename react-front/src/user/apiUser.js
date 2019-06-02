ad062a12ce84da532b03fc7a233d8234ac3d247d
jq1004 <junqingl@usc.edu>
2019-06-02 15:15:10 -0400

    implement unfollow

--- react-front/src/user/apiUser.js
+++ react-front/src/user/apiUser.js
@@ -82,6 +82,22 @@ export const follow = (userId, token, followId) => {
 		.catch(err => console.log(err));
 	};
 
+export const unfollow = (userId, token, unfollowId) => {
+		return fetch(`http://localhost:8080/user/unfollow`,{
+			method:'PUT',
+			headers: {
+				Accept:"application/json",
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`
+			},
+			body: JSON.stringify({userId, unfollowId})
+		})
+		.then(response => {
+			return response.json();
+		})
+		.catch(err => console.log(err));
+	};
+
 
 
 
