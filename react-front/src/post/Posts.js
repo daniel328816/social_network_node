6f719d8a3c436396503ee78d1044be621d551e59
jq1004 <junqingl@usc.edu>
2019-06-03 12:09:25 -0400

    show post's user date and excerpt

--- react-front/src/post/Posts.js
+++ react-front/src/post/Posts.js
@@ -22,26 +22,42 @@ class Posts extends Component {
 		});
 	}
  
-	renderPosts = (posts) => (
+	renderPosts = (posts) => {
+		return (
 			<div className="row"> 
 					{
-						posts.map((post,i) => (
-							<div className="card col-md-4" key={i}>
-						
-							  <div className="card-body">
-							    <h5 className="card-title">{post.title}</h5>
-							    <p className="card-text">{post.body}</p>
-							    <Link 
-							    	to={`/posts/${post._id}`} 
-							    	className="btn btn-raised btn-primary btn-sm"
-							    >
-							    	Read more
-							    </Link>
-							  </div>
-							</div>					
-					))}
+						posts.map((post,i) => {
+							const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
+							const posterName = post.postedBy ? post.postedBy.name : " Unknown";
+							return (
+
+								<div className="card col-md-4" key={i}>
+								  <div className="card-body">
+								    <h5 className="card-title">{post.title}</h5>
+								    <p className="card-text">{post.body.substring(0,100)}</p>
+								    <br />
+								    <p className="font-italic mark">
+								    	Posted by 
+								    	<Link to={`${posterId}`}>
+								    		{posterName}{" "}
+								    	</Link>
+								    	on {new Date(post.created).toDateString()}
+								    </p>
+								    <Link 
+								    	to={`/posts/${post._id}`} 
+								    	className="btn btn-raised btn-primary btn-sm"
+								    >
+								    	Read more
+								    </Link>
+								  </div>
+								</div>	
+							)
+					})}
 			</div>
-		)
+		);
+	};
+			
+		
 
 	render(){
 		const {posts} = this.state;
