1cbd62134c5576445a9865b04ea84a1d37bccb28
jq1004 <junqingl@usc.edu>
2019-06-03 11:35:50 -0400

    show all posts in home page

--- react-front/src/post/apiPost.js
+++ react-front/src/post/apiPost.js
@@ -12,3 +12,14 @@ export const create = (userId, token, post) => {
 		})
 		.catch(err => console.log(err));
 	};
+
+export const list = () => {
+	return fetch(`http://localhost:8080/posts`,{
+			method:'GET'
+		})
+		.then(response => {
+			return response.json();
+		})
+		.catch(err => console.log(err));
+
+};
